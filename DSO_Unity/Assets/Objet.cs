4a42de3b81248e4b013ec248a746665e7441e1f5
Onesmy <onesmy@hotmail.fr>
2014-11-07 18:41:44 +0100

    Création du projet
    Tous les fichiers créés automatiquement et la classe "Objet", qu'il
    risque de falloir modifier en fonction de la façon dont on veut gérer la
    position (gérée par un Vector2D ici) et le Sprite.

---
+++ DSO_Unity/Assets/Objet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Objet { // "Object" est une classe native, non ?
+
+	// Déclaration des attributs :
+	Sprite _sprite ;
+	Vector2 _position ;
+
+	// Constructeur :
+	public Objet (Sprite sprite, Vector2 position) {
+		this._sprite = sprite;
+		this._position = position;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
