df97cc8d69e8c19453f2d8bc0a30a7a00c5d7b79
Soussang <carlos0072@hotmail.com>
2014-11-15 12:02:24 -0500

    Ajout de la classe abstraite ennemy
    >> Possibilité de devoir ajouter une nouvelle classe abstraite d'effets
    continus
    >> Unity 4.5.5, Il se peut qu'il y ait incompatibilité avec les autres
    versions.

---
+++ DSO_Unity/Assets/Ennemy.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Ennemy : Object {
+	//Attributs privés
+	private string _nom;
+	private float _vitesse;
+	private int _pointDeVie;
+	private int _experience;
+	private int _argent;
+	//A implémenter
+	//private IList<StatusEffect> _effets;
+
+	//Get & set
+	public string Nom{
+		get	{ return this._nom;	}
+		set{ this._nom = value;	}
+	}
+
+	public float Vitesse{
+		get	{ return this._vitesse;	}
+		set{ this._vitesse = value;	}
+	}
+
+	public int PointDeVie{
+		get	{ return this._pointDeVie;	}
+		set{ this._pointDeVie = value;	}
+	}
+	public int Experience{
+		get	{ return this._experience;	}
+		set{ this._experience = value;	}
+	}
+	public int Argent{
+		get	{ return this._argent;	}
+		set{ this._argent = value;	}
+	}
+	//A implémenter
+	/*public string IList<StatusEffect> Effets{
+		get	{ return this._effets;	}
+		set{ this._effets = value;	}
+	}*/
+
+	//Constructeur
+	public Ennemy(string nom, float vitesse, int pointDeVie, int experience, int argent)
+	{
+		this.Nom = nom;
+		this.Vitesse = vitesse;
+		this.PointDeVie = pointDeVie;
+		this.Experience = experience;
+		this.Argent = argent;
+		//A implémenter
+		//this.Effets = new List<StatusEffect>;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
